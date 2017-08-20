35dbb8408bf3716e384f46361c1c18234701de1e
pau <paul.chen@sharemarking.com>
2017-08-20 21:26:32 +0800

    ss

---
+++ app.js
@@ -0,0 +1,11 @@
+var express=require("express");
+var app=express();
+app.get('/',function(req,res){
+    res.send('hello world!');
+});
+
+var server=app.listen(80,function(){
+    var host=server.address().address;
+    var port=server.address().port;
+    console.log('example app listing at http://%s:%s', host, port);
+})
