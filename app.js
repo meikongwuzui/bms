72dc4cf645db41d1d6598a63423e0cb12b406dce
meikongwuzui <865419440@qq.com>
2017-08-21 08:56:35 +0800

    listenn

--- app.js
+++ app.js
@@ -11,3 +11,9 @@ app.use(express.static('public'));
 app.use('/',routes);
 
 WeiXin.initMsg();
+
+var server=app.listen(80,function(){
+    var host=server.address().address;
+    var port=server.address().port;
+    console.log('example app listing at http://%s:%s', host, port);
+})
