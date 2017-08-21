50be5d4e7a21609178306ec0cab5cf6579648c82
meikongwuzui <865419440@qq.com>
2017-08-21 17:32:45 +0800

    enent message

--- routes/weixin.js
+++ routes/weixin.js
@@ -24,7 +24,26 @@ function resTextMsg(){
             funcFlag:0
         };
         weixinApi.sendMsg(resMsg);
-    })
+    });
+
+    // 监听事件消息
+    weixin.eventMsg(function(msg) {
+        console.log("eventMsg received");
+        console.log(JSON.stringify(msg));
+        var content="消息内容:"+msg.content+"\n";
+        content=content+"toUserName:"+msg.toUserName+"\n";
+        content=content+"fromUserName:"+msg.fromUserName+"\n";
+        
+        var resMsg={};
+        resMsg={
+            fromUserName:msg.toUserName,
+            toUserName:msg.fromUserName,
+            msgType:"event",
+            content:content,
+            funcFlag:0
+        };
+        weixinApi.sendMsg(resMsg);
+    });
 }
 
 module.exports=new WeiXin();
