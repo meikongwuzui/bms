1a40fa55f2fabaf28b8253102fdc6b5da9e8ce21
meikongwuzui <865419440@qq.com>
2017-08-22 16:44:31 +0800

    ss

--- routes/sql/booktype.js
+++ routes/sql/booktype.js
@@ -2,17 +2,19 @@ var sqlhelper=require('../sqlhelper');
 
 function b_booktype(){
     this.getlist=function(){
-       var result= sqlhelper.query('SELECT\
-        b_booktype.pkid,\
-        b_booktype.fkparentid,\
-        b_booktype.`name`,\
-        b_booktype.layer,\
-        b_booktype.sort,\
-        b_booktype.`status`\
-        FROM\
-        b_booktype\
-        ')
-        return result;
+    //    var result= sqlhelper.query('SELECT\
+    //     b_booktype.pkid,\
+    //     b_booktype.fkparentid,\
+    //     b_booktype.`name`,\
+    //     b_booktype.layer,\
+    //     b_booktype.sort,\
+    //     b_booktype.`status`\
+    //     FROM\
+    //     b_booktype\
+    //     ')
+    //     return result;
+    var result= sqlhelper.query('SELECT 1+1 as solution    ')
+    return result;
     }
 }
 
