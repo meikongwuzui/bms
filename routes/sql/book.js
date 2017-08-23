95dad969c8c528d2b3ee82e6d31f5ed487580dac
meikongwuzui <865419440@qq.com>
2017-08-23 12:46:54 +0800

    s

--- routes/sql/book.js
+++ routes/sql/book.js
@@ -8,11 +8,12 @@ function b_book(){
         b_book.author,\
         b_book.edition,\
         b_book.contentintroduction,\
-        b_book.bookcover\
+        b_book.bookcover,\
+        b_booktype.`name` AS typename\
         FROM\
         b_book\
         RIGHT OUTER JOIN b_type_book_ref ON b_book.pkid=b_type_book_ref.fkbookid\
-        LEFT OUTER JOIN b_booktype ON b_type_book_ref.fkbooktypeid=b_booktype.pkid\
+        RIGHT OUTER JOIN b_booktype ON b_type_book_ref.fkbooktypeid=b_booktype.pkid\
         WHERE b_booktype.pkid= '+typeid,
         function(result){
             console.log("booktype result:"+result);
