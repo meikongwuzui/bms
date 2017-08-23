ed5730b97f7f0473446e406e621a719b86dde914
pau <paul.chen@sharemarking.com>
2017-08-24 01:04:13 +0800

    sd

---
+++ routes/jssdk.js
@@ -0,0 +1,28 @@
+var http=require('http');  
+
+function Jssdk(){
+    this.getaccess_token=function(){
+        //get 请求外网  
+        http.get('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxa4e7517fe99f820e&secret=513a774504039197f29fcf486ddb32c5'
+        ,function(req,res){  
+            req.on('data',function(data){  
+                console.log(data);
+            });  
+            req.on('end',function(){  
+            });  
+        });  
+    },
+    this.getjsapi_ticket=function(){
+        //get 请求外网  
+        http.get('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxa4e7517fe99f820e&secret=513a774504039197f29fcf486ddb32c5'
+        ,function(req,res){  
+            req.on('data',function(data){  
+                 
+            });  
+            req.on('end',function(){  
+            });  
+        });  
+    }
+}
+
+module.exports=new Jssdk();
