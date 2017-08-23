c44eab43965bfd811b64719fda8d534707ebc8f6
pau <paul.chen@sharemarking.com>
2017-08-24 02:19:05 +0800

    df

--- routes/index.js
+++ routes/index.js
@@ -51,6 +51,22 @@ router.get('/book/detail',function(req,res){
     }
 })
 
+router.post('/book/add',function(req,res){
+    // 定义了一个post变量，用于暂存请求体的信息
+    var poststr = '';     
+
+   // 通过req的data事件监听函数，每当接受到请求体的数据，就累加到post变量中
+   req.on('data', function(chunk){    
+    poststr += chunk;
+   });
+
+   // 在end事件触发后，通过querystring.parse将post解析为真正的POST请求格式，然后向客户端返回。
+   req.on('end', function(){    
+       console.log(poststr);
+       res.end(util.inspect(poststr));
+   });
+})
+
 router.get('/weixin/getjsticket',function(req,res){
     jssdk.getaccess_token(function(result){
         jssdk.getjsapi_ticket(JSON.parse(result).access_token,function(data){
