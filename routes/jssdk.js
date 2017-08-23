e21d67be5d07c343547ca57f3b4ca8d99a6ed1b6
pau <paul.chen@sharemarking.com>
2017-08-24 01:23:45 +0800

    df

--- routes/jssdk.js
+++ routes/jssdk.js
@@ -3,13 +3,23 @@ var http=require('https');
 function Jssdk(){
     this.getaccess_token=function(){
         //get 请求外网  
-        http.get('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxa4e7517fe99f820e&secret=513a774504039197f29fcf486ddb32c5'
-        ,function(req,res){  
-            req.on('data',function(data){  
-                console.log( JSON.stringify(data));
+        var url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxa4e7517fe99f820e&secret=513a774504039197f29fcf486ddb32c5';
+        http.get(url, function (res) {  
+            var datas = [];  
+            var size = 0;  
+            res.on('data', function (data) {  
+                datas.push(data);  
+                size += data.length;  
+            //process.stdout.write(data);  
             });  
-            req.on('end',function(){  
+            res.on("end", function () {  
+                var buff = Buffer.concat(datas, size);  
+                var result = iconv.decode(buff, "utf8");//转码//var result = buff.toString();//不需要转编码,直接tostring  
+                console.log(result);  
             });  
+        }).on("error", function (err) {  
+            Logger.error(err.stack)  
+            callback.apply(null);  
         });  
     },
     this.getjsapi_ticket=function(){
