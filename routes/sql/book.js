fb25fed5152825be4f4882462756029793dac542
meikongwuzui <865419440@qq.com>
2017-08-25 10:44:25 +0800

    sd

--- routes/sql/book.js
+++ routes/sql/book.js
@@ -36,8 +36,8 @@ function b_book(){
         para,
         function(result){
             console.log('sql result:'+ JSON.stringify( result));
-            console.log('sql result:'+ JSON.stringify( result)[0].count);
-            callback(JSON.stringify( result)[0].count>0);
+            console.log('sql result:'+ result[0].count);
+            callback(result[0].count>0);
         });
     }
 }
