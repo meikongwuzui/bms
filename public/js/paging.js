2dcca34c481a7e53e2eb696631167678e19d948a
pau <paul.chen@sharemarking.com>
2017-08-28 23:21:00 +0800

    bug

--- public/js/paging.js
+++ public/js/paging.js
@@ -1,6 +1,6 @@
 window.onload = function(){ 
-    var transhref=  location.protocol + '//' + location.hostname + '/' + location.pathname;
-    $('ul.am-pagination li.cantransform').on('click',function(){
+    var transhref=  location.protocol + '//' + location.hostname + location.pathname;
+    $('ul.am-pagination li.cantransform a').on('click',function(){
       var page = $(this).attr('data-href');
       location.href= transhref + '?currentpage='+page+'&pagesize=3&orderby=pkid&ordertype=desc';
     })
