b2714fd7c9743674a55aca35fe4a44e9d9ceda20
pau <paul.chen@sharemarking.com>
2017-08-28 23:16:40 +0800

    页面加载完成

--- public/js/paging.js
+++ public/js/paging.js
@@ -1,7 +1,7 @@
-$(function(){
+$(document).ready(function(){ 
     var transhref=  location.protocol + '//' + location.hostname + '/' + location.pathname;
     $('ul.am-pagination li.cantransform').on('click',function(){
       var page = $(this).attr('data-href');
       location.href= transhref + '?currentpage='+page+'&pagesize=3&orderby=pkid&ordertype=desc';
     })
-  })
+  });
