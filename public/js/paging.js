35d398ac51fc74149665cc408ceba174043c80cc
pau <paul.chen@sharemarking.com>
2017-08-28 23:18:14 +0800

    d

--- public/js/paging.js
+++ public/js/paging.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){ 
+window.onload = function(){ 
     var transhref=  location.protocol + '//' + location.hostname + '/' + location.pathname;
     $('ul.am-pagination li.cantransform').on('click',function(){
       var page = $(this).attr('data-href');
       location.href= transhref + '?currentpage='+page+'&pagesize=3&orderby=pkid&ordertype=desc';
     })
-  });
+  };
